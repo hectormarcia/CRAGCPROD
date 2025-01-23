34dc7c50453844a21a409677e46fcc14d4116857
Schalk Burger <schalk.burger@supplychainpartner.com>
2025-01-23 12:54:28 +0200

    add allowed hosts setting

--- gc_cra/settings.py
+++ gc_cra/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-z6iijd7dy0h*^x#=if78+mn(v(e$etd@f_71xpoj&lt(cwe$dl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "*" ]
 
 # Application definition
 
