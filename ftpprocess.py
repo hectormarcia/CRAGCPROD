636d78de8e11cbfe42cc4c2e59ce35e650f803c3
Schalk Burger <schalk.burger@supplychainpartner.com>
2025-02-02 07:48:25 +0200

    ftpjob

---
+++ ftpprocess.py
@@ -0,0 +1,16 @@
+import requests
+
+url = r'https://qacra-asfrgjg5afbyc8e3.northeurope-01.azurewebsites.net/ftp'
+payload = {'process':'true'} # add paramaters to the dictionary as needed
+
+# GET Only
+r = requests.get(url)
+
+# GET with parameters
+r = requests.get(url, params=payload)
+
+
+# URL, Response and Status
+print(r.url)
+print(r.text)
+#print(r.status_code‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍)
