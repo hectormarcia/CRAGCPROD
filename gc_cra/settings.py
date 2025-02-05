ac033e3558c27006b6ce98e76d03fddf0f49ca2b
Schalk Burger <schalk.burger@supplychainpartner.com>
2025-02-05 11:07:22 +0200

    update for django admin

--- gc_cra/settings.py
+++ gc_cra/settings.py
@@ -194,6 +194,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 # to enable whitenoise
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
