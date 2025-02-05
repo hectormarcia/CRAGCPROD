fd546c3123c979bdc9ab72b375a82a6d3435b420
Schalk Burger <schalk.burger@supplychainpartner.com>
2025-02-05 12:06:54 +0200

    admin update

--- panel/admin.py
+++ panel/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from models import ProgramSequence, CraFtpLog, CRAstatus
 
-
 @admin.register(ProgramSequence)
 class ProgramSequenceAdmin(admin.ModelAdmin):
     list_display = ('programname', 'sequence')     
