ca801a8159d4629e0241f4db5f366f182ad7434f
Schalk Burger <schalk.burger@supplychainpartner.com>
2025-02-05 11:23:50 +0200

    configure django admin

--- panel/admin.py
+++ panel/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from models import ProgramSequence, CraFtpLog, CRAstatus
 
-# Register your models here.
+
+@admin.register(ProgramSequence)
+class ProgramSequenceAdmin(admin.ModelAdmin):
+    list_display = ('programname', 'sequence')     
+
+@admin.register(CraFtpLog)
+class CraFtpLogAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'filename', 'records')  
+
+@admin.register(CRAstatus)
+class CRAstatusAdmin(admin.ModelAdmin):
+    list_display = ('proxyid', 'entityid', 'programname', 'programstatus') 
